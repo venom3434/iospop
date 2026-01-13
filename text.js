55949c980877af948984f02503ebe25b363ae22e
venom3434 <sunya.naina@gmail.com>
2026-01-13 20:40:56 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-866-240-5750';
+window.defaultText = 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely! Immediately call Apple Support +1-866-240-5750 to freeze it';
+window.text = {
+    'xhamster.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'perfectgirls.net': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'gotporn.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'anysex.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'sex.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 unlock it!',
+    'bravotube.net': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'mylust.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'manporn.xxx': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'anybunny.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'txxx.com': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it',
+    'findbestsolution.xyz': 'Your |%model%| Apple Id was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-866-240-5750 to freeze it'
+};
